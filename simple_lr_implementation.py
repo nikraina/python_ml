47b39cd8c09203459e7b90bf0af8b37f6387344b
Nikhil Raina <nraina@tesla.com>
2016-07-08 13:44:34 -0700

    add random dataset generator

--- simple_lr_implementation.py
+++ simple_lr_implementation.py
@@ -2,13 +2,27 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('ggplot')
 
-xs = np.array([1,2,3,4,5,6,7], dtype=np.float64)
-ys = np.array([5,6,7,8,9,2,3], dtype=np.float64)
+# xs = np.array([1,2,3,4,5,6,7], dtype=np.float64)
+# ys = np.array([5,6,7,8,9,2,3], dtype=np.float64)
 
+def create_dataset(hm,variance,step=2,correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance,variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val+=step
+        elif correlation and correlation == 'neg':
+            val-=step
 
+    xs = [i for i in range(len(ys))]
+    
+    return np.array(xs, dtype=np.float64),np.array(ys,dtype=np.float64)
 
 def best_fit_slope_intercept(xs,ys):
 	m = (( (mean(xs)*mean(ys)) - mean(xs*ys) ) / 
@@ -25,6 +39,8 @@ def coef_of_determination(ys_orig, ys_line):
 	squared_error_y_mean = squared_error(ys_orig, y_mean_line)
 	return 1 - (squared_error_regr/squared_error_y_mean)
 
+
+xs, ys = create_dataset(40,40,2,correlation='pos')
 slope, intercept = best_fit_slope_intercept(xs,ys)
 
 regression_line = [ (slope*x)+intercept for x in xs]
