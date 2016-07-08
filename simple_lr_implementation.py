39dc58b42c30e8e8aa0870a37c44edf9f4704041
Nikhil Raina <nraina@tesla.com>
2016-07-08 13:20:01 -0700

    add code for r_squared error calculation

--- simple_lr_implementation.py
+++ simple_lr_implementation.py
@@ -1,19 +1,38 @@
 from statistics import mean
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from matplotlib import style
 
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,6,7,8,9,5], dtype=np.float64)
+style.use('ggplot')
 
-# plt.scatter(xs,ys)
-# plt.show()
+xs = np.array([1,2,3,4,5,6,7], dtype=np.float64)
+ys = np.array([5,6,7,8,9,2,3], dtype=np.float64)
 
-def best_fit_slope(xs,ys):
+
+
+def best_fit_slope_intercept(xs,ys):
 	m = (( (mean(xs)*mean(ys)) - mean(xs*ys) ) / 
 		( (mean(xs)**2) - (mean(xs**2)) ))
-	return m
+	b = mean(ys) - m*mean(xs)
+	return m,b
 
+def squared_error(ys_orig, ys_line):
+	return sum((ys_line - ys_orig)**2)
 
-slope = best_fit_slope(xs,ys)
+def coef_of_determination(ys_orig, ys_line):
+	y_mean_line = [mean(ys_orig) for y in ys_orig]
+	squared_error_regr = squared_error(ys_orig, ys_line)
+	squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+	return 1 - (squared_error_regr/squared_error_y_mean)
 
-print(slope)
+slope, intercept = best_fit_slope_intercept(xs,ys)
+
+regression_line = [ (slope*x)+intercept for x in xs]
+
+r_squared = coef_of_determination(ys,regression_line)
+
+print(slope, intercept, r_squared)
+
+plt.scatter(xs,ys)
+plt.plot(xs,regression_line)
+plt.show()
